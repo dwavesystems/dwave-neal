6f954a2995cbb72455f8d1c754e3b79fb5745d8c
William Bernoudy <wbernoudy@dwavesys.com>
2017-12-04 16:06:39 -0800

    Trying to fix python version on travis again

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,8 +2,8 @@
 set -e -x
 
 for PYBIN in /opt/python/*/bin; do
-    if [[ ${PYBIN} =~ 2.6|3.3 ]]; then
-        # numpy doesn't support 2.6 or 3.3, just skip em
+    if [[ ${PYBIN} =~ 26|2.6|33|3.3 ]]; then
+        # numpy doesn't support 2.6 or 3.3, just skip them
         continue
     fi
     "${PYBIN}/pip" install -r /io/requirements.txt
@@ -22,8 +22,8 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    if [[ ${PYBIN} =~ 26|33 ]]; then
-        # numpy doesn't support 2.6 or 3.3, just skip em
+    if [[ ${PYBIN} =~ 26|2.6|33|3.3 ]]; then
+        # numpy doesn't support 2.6 or 3.3, just skip them
         continue
     fi
     "${PYBIN}/pip" install dwave_neal --no-index -f /io/wheelhouse/
