f4200ed88b8e3bfc87fb0589acecd622932ba197
William Bernoudy <wbernoudy@dwavesys.com>
2017-12-05 12:08:49 -0800

    Different approach to unit tests on travis

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -21,11 +21,12 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
+cd /io/
 for PYBIN in /opt/python/*/bin/; do
     if [[ ${PYBIN} =~ 26|2.6|33|3.3 ]]; then
         # numpy doesn't support 2.6 or 3.3, just skip them
         continue
     fi
     "${PYBIN}/pip" install dwave_neal --no-index -f /io/wheelhouse/
-    (set -e; cd /io/; "${PYBIN}/python" -m unittest discover)
+    "${PYBIN}/python" -m unittest discover
 done
