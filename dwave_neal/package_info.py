a8d932f00b146477044dd7fccd35dd2fca6a4f49
Alexander Condello <acondello@dwavesys.com>
2018-05-16 10:30:21 -0700

    Refactor setup.py

---
+++ dwave_neal/package_info.py
@@ -0,0 +1,4 @@
+__version__ = '0.2.0'
+__author__ = 'D-Wave Systems Inc.'
+__authoremail__ = 'wbernoudy@dwavesys.com'
+__description__ = 'General Ising graph simulated annealing solver'
--- setup.py
+++ setup.py
@@ -1,35 +1,87 @@
 from setuptools import setup, Extension
-from Cython.Distutils import build_ext
-import platform
-import numpy
+from setuptools.command.build_ext import build_ext
+import sys
+import os
 
-src_folder = "dwave_neal/"
+import numpy as np
 
-cpp_version_arg = "-std=c++11" # default, works on linux
+cwd = os.path.abspath(os.path.dirname(__file__))
+if not os.path.exists(os.path.join(cwd, 'PKG-INFO')):
+    try:
+        from Cython.Build import cythonize
+        USE_CYTHON = True
+    except ImportError:
+        USE_CYTHON = False
+else:
+    USE_CYTHON = False
 
-if platform.system().lower() == "windows":
-    cpp_version_arg = "/std:c++14"
+extra_compile_args = {
+    'msvc': ['/std:c++14'],
+    'unix': ['-std=c++11'],
+}
 
-ext_module = Extension("dwave_neal_sampler", 
-        [src_folder + "sampler/" + "general_simulated_annealing.pyx", 
-            src_folder + "sampler/" + "cpu_sa.cpp"], 
-        language="c++",
-        extra_compile_args=[cpp_version_arg],
-        extra_link_args=[cpp_version_arg],
-        include_dirs=[numpy.get_include()]
-        )
+extra_link_args = {
+    'msvc': [],
+    'unix': [],
+}
 
-setup(name="dwave_neal",
-      version="0.2.0",
-      description="General Ising graph simulated annealing solver",
-      author="William Bernoudy",
-      author_email="wbernoudy@dwavesys.com",
-      license="Apache 2.0",
-      cmdclass = {"build_ext": build_ext}, 
-      ext_modules = [ext_module],
-      packages=["dwave_neal", "dwave_neal.dwave_neal_dimod"],
-      install_requires=[
-          "numpy",
-          "dimod",
-      ])
 
+class build_ext_compiler_check(build_ext):
+    def build_extensions(self):
+        compiler = self.compiler.compiler_type
+
+        compile_args = extra_compile_args[compiler]
+        for ext in self.extensions:
+            ext.extra_compile_args = compile_args
+
+        link_args = extra_compile_args[compiler]
+        for ext in self.extensions:
+            ext.extra_compile_args = link_args
+
+        build_ext.build_extensions(self)
+
+
+ext = '.pyx' if USE_CYTHON else '.cpp'
+
+
+extensions = [Extension(
+    name="dwave_neal_sampler",
+    sources=["./dwave_neal/sampler/general_simulated_annealing" + ext],
+    include_dirs=[np.get_include()],
+    language='c++',
+)]
+
+if USE_CYTHON:
+    extensions = cythonize(extensions, language='c++')
+
+packages = ['dwave_neal',
+            'dwave_neal_dimod',
+            ]
+
+install_requires = ['dimod==0.3.1',
+                    'numpy==1.13.0',
+                    ]
+
+_PY2 = sys.version_info.major == 2
+
+# add __version__, __author__, __authoremail__, __description__ to this namespace
+if _PY2:
+    execfile("./dwave_neal/package_info.py")
+else:
+    exec(open("./dwave_neal/package_info.py").read())
+
+
+setup(
+    name='dwave_neal',
+    version=__version__,
+    author=__author__,
+    author_email=__authoremail__,
+    description=__description__,
+    long_description=open('README.md').read(),
+    url='https://github.com/dwavesystems/dwavebinarycsp',
+    license='Apache 2.0',
+    packages=packages,
+    install_requires=install_requires,
+    ext_modules=extensions,
+    cmdclass={'build_ext': build_ext_compiler_check}
+)
