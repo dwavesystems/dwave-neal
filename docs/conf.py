359e2970d86c04d1ca92b1de5e8d487fc0e05b8f
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2018-09-17 15:24:48 -0700

    Fix for sphinx-build on Windows

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
-# sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # -- General configuration ------------------------------------------------
 # import sphinx
