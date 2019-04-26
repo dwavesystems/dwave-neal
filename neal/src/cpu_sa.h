96a0225d1a33c6fcd1be4e8b85cca2416d166967
Radomir Stevanovic <radomir@dwavesys.com>
2019-04-25 17:00:19 -0700

    Simulate thread_local on older MSVC comilers

--- neal/src/cpu_sa.h
+++ neal/src/cpu_sa.h
@@ -18,9 +18,15 @@
 #define _cpu_sa_h
 
 #ifdef _MSC_VER
-// add uint64_t definition for windows
-typedef __int64 int64_t;
-typedef unsigned __int64 uint64_t;
+    // add uint64_t definition for windows
+    typedef __int64 int64_t;
+    typedef unsigned __int64 uint64_t;
+
+    // add thread_local (C++11) support for MSVC 9 and 10 (py2.7 and py3.4 on win)
+    // note: thread_local support was added in MSVC 14 (Visual Studio 2015, MSC_VER 1900)
+    #if _MSC_VER < 1900
+    #define thread_local __declspec(thread)
+    #endif
 #endif
 
 double get_flip_energy(
