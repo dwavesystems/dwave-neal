9f02e465831dcafea570eacab2c6a6a5c0a7964d
Alexander Condello <arcondello@gmail.com>
2020-02-10 09:07:12 -0800

    Update version 0.5.2 -> 0.5.3
    Changes
    -------
    * Drop support for python 2.7 and 3.4
    
    Fixes
    -----
    * Loosened NumPy version requirements

--- neal/package_info.py
+++ neal/package_info.py
@@ -14,7 +14,7 @@
 #
 # ================================================================================================
 
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'wbernoudy@dwavesys.com'
 __description__ = 'General Ising graph simulated annealing solver'
