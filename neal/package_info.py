4c5383d6716db43b0ac9a9c6b48ff8d9d741952f
Radomir Stevanovic <radomir@dwavesys.com>
2020-09-17 04:35:48 -0700

    Release version 0.5.5
    New
    ---
    
    - Accept samples-like for `initial_states` (switched to
      `dimod.Initialized` ABC)
    
    Fixes
    ---
    
    - Updated Windows tests to work with latest AppVeyor setup
    - Stopped using hierarchical requirements include

--- neal/package_info.py
+++ neal/package_info.py
@@ -14,7 +14,7 @@
 #
 # ================================================================================================
 
-__version__ = '0.5.4'
+__version__ = '0.5.5'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'wbernoudy@dwavesys.com'
 __description__ = 'General Ising graph simulated annealing solver'
