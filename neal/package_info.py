a11e477c3a6b3585d75ee8b58be75a4127d0c17c
Alexander Condello <arcondello@gmail.com>
2021-12-21 11:56:55 -0800

    Update version 0.5.8 -> 0.5.9

--- neal/package_info.py
+++ neal/package_info.py
@@ -14,7 +14,7 @@
 #
 # ================================================================================================
 
-__version__ = '0.5.8'
+__version__ = '0.5.9'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'wbernoudy@dwavesys.com'
 __description__ = 'General Ising graph simulated annealing solver'
